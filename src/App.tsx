bdc101314d882102d3da1dc0aad91df996fc7b62
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-08-04 00:38:21 +0600

    ant-design layout added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import MainLayout from "./components/layout/MainLayout"
 
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
   return (
     <>
 
-      <h1>Vite + React</h1>
+      <MainLayout></MainLayout>
 
    
     </>
